bb78a30aaa809105a9bae612b53c24c9ffa75a9a
Nassim LOUNADI <nassimlnd37@gmail.com>
2022-03-12 21:13:39 +0100

    Fix connection bug

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,7 @@ $curPageName = substr($_SERVER["SCRIPT_NAME"], strrpos($_SERVER["SCRIPT_NAME"], 
 if ($curPageName == "index.php") {
 ?>
     <header>
-        <a href="index.php"><img src="images/logo_gif.gif" alt="Logo" id="logo"></a>
+        <a href="index.php"><img src="images/logo.png" alt="Logo" id="logo"></a>
         <nav>
             <ul class="nav__links">
                 <li><a href="index.php">Accueil</a></li>
--- pages/pari.php
+++ pages/pari.php
@@ -1,6 +1,7 @@
 <?php
+session_start();
 if (!isset($_SESSION['user'])) {
-    header("Location: login.php?log_error=notconnected");
+    echo $_SESSION['user'];
 }
 ?>
 <!DOCTYPE html>
