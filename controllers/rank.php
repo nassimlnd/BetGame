e583a89fbb5b298df9d8d8fa378de9b7796aa6c6
Nassim LOUNADI <nassimlnd37@gmail.com>
2022-03-20 21:43:21 +0100

    Continue dev betting history

--- controllers/rank.php
+++ controllers/rank.php
@@ -1,63 +1,61 @@
 <?php
 //rank definition :
-    function attributerank($ranknum){
-        if(isset($ranknum)){
-            switch($ranknum){
+function attributerank($ranknum)
+{
+    if (isset($ranknum)) {
+        switch ($ranknum) {
 
-                case $ranknum <= 100:
-                    $rankdesc = "iron";
-                    return $rankdesc;
-                    break;
-                
-                case $ranknum < 200 && $ranknum >= 100:
-                    $rankdesc = "bronze";
-                    return $rankdesc;
-                    break;
-     
-                case $ranknum < 300 && $ranknum >= 200:
-                    $rankdesc = "silver";
-                    return $rankdesc;
-                    break;
-                
-                case $ranknum < 400 && $ranknum >= 300:
-                    $rankdesc = "gold";
-                    return $rankdesc;
-                    break;
-    
-                case $ranknum < 500 && $ranknum >= 400:
-                    $rankdesc = "gold";
-                    return $rankdesc;
-                    break;
-    
-                case $ranknum < 600 && $ranknum >= 500:
-                    $rankdesc = "platinium";
-                    return $rankdesc;
-                    break;
-    
-                case $ranknum < 700 && $ranknum >= 600:
-                    $rankdesc = "diamond";
-                    return $rankdesc;
-                    break;
-                
-                case $ranknum < 800 && $ranknum >= 700:
-                    $rankdesc = "platinium";
-                    return $rankdesc;
-                    break;
-                
-                case $ranknum < 1000 && $ranknum >= 800:
-                    $rankdesc = "immortal";
-                    return $rankdesc;
-                    break;
-    
-                case $ranknum >= 1000 :
-                    $rankdesc = "radiant";
-                    return $rankdesc;
-                    break;
-            }
+            case $ranknum <= 100:
+                $rankdesc = "iron";
+                return $rankdesc;
+                break;
 
-        } else {
-            return " ";
+            case $ranknum < 200 && $ranknum >= 100:
+                $rankdesc = "bronze";
+                return $rankdesc;
+                break;
+
+            case $ranknum < 300 && $ranknum >= 200:
+                $rankdesc = "silver";
+                return $rankdesc;
+                break;
+
+            case $ranknum < 400 && $ranknum >= 300:
+                $rankdesc = "gold";
+                return $rankdesc;
+                break;
+
+            case $ranknum < 500 && $ranknum >= 400:
+                $rankdesc = "gold";
+                return $rankdesc;
+                break;
+
+            case $ranknum < 600 && $ranknum >= 500:
+                $rankdesc = "platinium";
+                return $rankdesc;
+                break;
+
+            case $ranknum < 700 && $ranknum >= 600:
+                $rankdesc = "diamond";
+                return $rankdesc;
+                break;
+
+            case $ranknum < 800 && $ranknum >= 700:
+                $rankdesc = "platinium";
+                return $rankdesc;
+                break;
+
+            case $ranknum < 1000 && $ranknum >= 800:
+                $rankdesc = "immortal";
+                return $rankdesc;
+                break;
+
+            case $ranknum >= 1000:
+                $rankdesc = "radiant";
+                return $rankdesc;
+                break;
         }
-        
-} 
-?>
+    } else {
+        return " ";
+    }
+}
