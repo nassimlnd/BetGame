11d6cb580f7937219898c83408d60fb92e329d41
sir-martino <martin.neron@laposte.net>
2022-05-08 00:45:26 +0200

    ma lubulule

--- pages/404.php
+++ pages/404.php
@@ -1,5 +1,14 @@
 <title>Erreur 404 - BetGame</title>
 
 <div class="error">
-    <h1> Error 404 : File not found! </h1>
+    <h1 id="404"> 404 </h1>
 </div>
+
+<div class ="errorQuote">
+     <h1>This is not the page you are looking for. </h1>
+</div>
+
+
+    <a href="index.php" class ="errorButton"> Revenir à l'accueil </a>
+    
+
